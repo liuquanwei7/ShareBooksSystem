102ee7bdaa3e741cd7ae492802a59c27f2589cdf
liu <1353965910@qq.com>
2018-06-07 14:38:17 +0800

    增加bookManagement包

---
+++ ShareBooksSystem/src/com/sharebookssystem/bookManagement/service/impl/BookManagementServiceImpl.java
@@ -0,0 +1,38 @@
+package com.sharebookssystem.bookManagement.service.impl;
+
+import com.sharebookssystem.bookManagement.dao.hibernate.BookManagementDaoImpl;
+import com.sharebookssystem.bookManagement.service.BookManagementService;
+import com.sharebookssystem.model.Manager;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 实现类
+ * 2018/6/5
+ */
+
+@Service
+public class BookManagementServiceImpl implements BookManagementService {
+    @Resource
+    private BookManagementDaoImpl dao;
+
+    public BookManagementServiceImpl() {
+
+    }
+
+    public BookManagementDaoImpl getDao() {
+        return dao;
+    }
+
+    public void setDao(BookManagementDaoImpl dao) {
+        this.dao = dao;
+    }
+
+
+    //判断管理员账户是否存在
+
+    public boolean isExistManager(Manager manager){
+        return dao.queryManager(manager);
+    }
+}
